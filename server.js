ba3b7aa712a02b8e472d39166741edef9a8cdbcf
Singularity3 <chko2775@colorado.edu>
2017-04-18 19:34:49 -0600

    Added info

--- server.js
+++ server.js
@@ -141,17 +141,17 @@ function generateTeams() {
 	for(var i = 0; i<red.length; i++) {
 		if(innos > 0){
 			playerList[red[i]].role = "inno";
-            playerList[red[i]].info = "You are a red team member. Destroy the red team by any means necessary. Tap this message to make it vanish."
+            playerList[red[i]].info = "You are a red team member. Destroy the blue team by any means necessary. Tap this message to make it vanish."
 			innos--;
 		}
 		else if(spies > 0) {
 			playerList[red[i]].role = "spy";
-            playerList[red[i]].info = "You are a red spy. Destroy the red team by any means necessary. Tap your name to change what color you appear as, to fool your enemies. Tap this message to make it vanish."
+            playerList[red[i]].info = "You are a red spy. Destroy the blue team by any means necessary. Tap your name to change what color you appear as, to fool your enemies. Tap this message to make it vanish."
 			spies--;
 		}
 		else if(doctors > 0) {
 			playerList[red[i]].role = "doctor";
-            playerList[red[i]].info = "You are a red doctor. Destroy the red team by any means necessary. Try and save one of your teammates with your antidote. Tap this message to make it vanish."
+            playerList[red[i]].info = "You are a red doctor. Destroy the blue team by any means necessary. Try and save one of your teammates with your antidote. Tap this message to make it vanish."
 			doctors--;
 		}
 	}
@@ -165,8 +165,9 @@ function startGame() {
 		io.in(playerList[i].id).emit('players', playerList);
 	}
     started = true;
-    setTimeout(warning, 420000);
-	setTimeout(endGame, 480000);
+    setTimeout(generateMessages, 60000)
+    setTimeout(warning, 240000);
+	setTimeout(endGame, 300000);
 }
 
 function warning() {
@@ -175,4 +176,33 @@ function warning() {
 
 function endGame() {
 	io.emit('gameOver', playerList);
+}
+
+function generateMessages() {
+    
+    for(var i = 0; i<playerList.length; i++){
+        var rpl = Math.floor(Math.random()*playerList.length);
+        var rpl2 = Math.floor(Math.random()*playerList.length);
+        while(rpl == i){
+            rpl = Math.floor(Math.random()*playerList.length);
+        }
+        while(rpl2 == i || rpl2 == rpl){
+            rpl2 = Math.floor(Math.random()*playerList.length);
+        }
+        var str1 = "Try having a chat with " + playerList[rpl].name; + " and see if you can get some information.";
+        var str2 = "See if you can get a look at " + playerList[rpl].name; + "'s screen and find out what team they are.";
+        var str3 = "Hold on to your poison until you can talk to your team members and figure out who they poisoned.";
+        var str4 = "Keep a close eye on " + playerList[rpl.name] + "; see if you can find out if they're a spy."
+        if(playerList.length > 12){
+            if(playerList[rpl].team == playerList[rpl2].team){
+                str3 = "I think that " + playerList[rpl].name + " and " + playerList[rpl2].name + " are on the same team; see if you can find out what's going on there."
+            }
+            else {
+                str3 = "I think that " + playerList[rpl].name + " and " + playerList[rpl2].name + " are on different teams; see if you can find out what's going on there."
+            }
+        }
+        var strArray = [str1, str2, str3, str4];
+        playerList[i].info = strArray[Math.floor(Math.random*4)];
+        io.in(playerList[i].id).emit('info', playerList[i].info);
+    }
 }
